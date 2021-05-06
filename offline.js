d68467b52d8966c6ca414c79dd8a02cd8d9c01b9
islam237834 <63165930+islam237834@users.noreply.github.com>
2021-05-06 16:55:46 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1620309254,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/tilemap.png",
+		"images/spawner-sheet0.png",
+		"images/destroyer-sheet0.png",
+		"images/spike-sheet0.png",
+		"images/spritefont.png",
+		"images/saw-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
